3ed23f0fc5cb605cbbfaddd4bd5364a96eba1981
CesareFortunato <cesare.fortunato89@gmail.com>
2026-05-11 17:44:48 +0200

    change api.php with graph

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StoryApiController;
 use App\Http\Controllers\Api\NodeApiController;
+use App\Http\Controllers\Api\StoryGraphController;
 
 Route::get('/stories', [StoryApiController::class, 'index']);
 Route::get('/stories/{story}', [StoryApiController::class, 'show']);
 Route::get('/stories/{story}/start', [StoryApiController::class, 'start']);
 
 Route::get('/nodes/{node}', [NodeApiController::class, 'show']);
+Route::get('/stories/{story}/graph', [StoryGraphController::class, 'show']);
